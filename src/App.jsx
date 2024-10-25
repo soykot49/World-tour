2bdc45d924d0d7063f58079075c364fae49a8116
soykot49 <mdrezwannurrashid26@gmail.com>
2024-10-26 05:18:13 +0600

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Countries from './component/Countries'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       
+      <h1>React World Tour</h1>
+      <Countries></Countries>
+    
+      
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Countries.jsx
@@ -0,0 +1,71 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from "./country";
+
+
+const Countries = () => {
+    const[countries, setCountries]=useState([])
+    const [visitedCountries , setVisitedCountries]=useState([])
+    const[visitedFlags, setVisitedFlags]=useState([])
+
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data)
+        )
+    },[])
+
+
+    const handleVisitedCountry=country=>{
+        console.log('Add this to your country');
+        
+        // console.log(country);
+        const newVisitedCountries=[...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+     
+    }
+
+    const handleVisitedFlags = flag =>{
+        // console.log('flag added');
+        const newVisitedFlags=[...visitedFlags , flag]
+        setVisitedFlags(newVisitedFlags)
+        
+    }
+
+
+
+    return (
+        <div>
+            <h3>Countries: {countries.length}</h3>
+
+            <div>
+            <h5>Visited countries:{visitedCountries.length}</h5>
+            <ul>
+                {
+                    visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                }
+            </ul>
+
+            </div>
+            {/* display country list */}
+            <div className="flag-container">
+                {
+                    visitedFlags.map((flag,idx) => <img key={idx} src={flag}></img>)
+                }
+            </div>
+         
+            <div  className="country-container">
+            {
+                countries.map(country => <Country key={country.cca3}
+                    handleVisitedCountry={handleVisitedCountry}
+                    handleVisitedFlags={handleVisitedFlags}
+                  country={country}></Country>) 
+            }
+
+            </div>
+           
+        </div>
+    );
+};
+
+export default Countries;
